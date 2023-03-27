df43889f4fa669fbb8257355317f7656adb834d8
Mateo Vasco <macv9620@utp.edu.co>
2023-03-26 21:16:08 -0500

    Restructuracion de carpetas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-//Middleware para poder recibir JSON
+
 app.use(express.json());
 const port = 3000;
-const routerApi = require('./routes/main.routes');
+const routerApi = require("./routes/main.routes");
 
-app.get('/', (req, res) => {
-  res.send('Bienvenido a ProductAPI, consulta el contrato de la API');
+app.get("/", (req, res) => {
+  res.send("Bienvenido a ProductAPI, consulta el contrato de la API");
 });
 
-
 app.listen(port, () => {
-  console.log('Escuchando en puerto: ' + port);
+  console.log("Escuchando en puerto: " + port);
 });
 
 routerApi(app);
